ea91b7aa9888cd29f94126615841d2704c8ddef6
Katharina Pittrich <k.pittrich@gmail.com>
2025-06-12 11:26:53 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,9 +10,6 @@ templates = ["plotly", "plotly_white", "plotly_dark", "ggplot2", "seaborn", "sim
 # Create a form for user input
 with st.form("User input"):
     url = st.text_input("Enter URL")
-    chunks = st.number_input("How many sentences would you like to group for plotting?", min_value=1, step=1)
-    template = st.selectbox("Choose a plotting template:", options=templates)  
-    
     submitted = st.form_submit_button("Submit")
 
 # If form is submitted, ensure inputs are valid
@@ -20,8 +17,6 @@ if submitted:
     if url and chunks:
         input_data = {
             "url": url,
-            "chunks": chunks,
-            "template": template
         }
         st.write("Your input:", input_data)
     else:
