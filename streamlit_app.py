40ed0c65763ce9b634bb78b988077acc67fee042
Katharina Pittrich <k.pittrich@gmail.com>
2025-06-12 12:33:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-import pandas as pd
-import plotly.graph_objects as go
+#import pandas as pd
+#import plotly.graph_objects as go
 
 # Initialize page state
 if "page" not in st.session_state:
